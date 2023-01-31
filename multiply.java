345785215c89088736995c83b1bf7666efbd1cba
Sravan Kumar Dubasi <sravankumardubasi@gmail.com>
2023-01-31 19:03:55 +0530

    added multiply

--- multiply.java
+++ multiply.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class multiply {
+public class multiply {
+    public static void main(String args[]){
+        int a=8;
+        int b=0;
+        System.out.println(a*b);
+
+    }
 }
